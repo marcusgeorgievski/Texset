b96a90dfd0f20d784cccc68d5ebff283f8169312
marcusgeorgievski <marcusgeorgievski@outlook.com>
2026-01-17 21:16:03 -0500

    Theme (light, dark, system): provider and toggle dropdown

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { geistSans } from "@/lib/fonts";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
   title: "Texset",
@@ -13,8 +14,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.className} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          // enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+
 export default function Home() {
-  return <div className="p-10">Texset</div>;
+  return (
+    <div className="p-10 flex gap-10">
+      Texset
+      <ThemeToggle />
+    </div>
+  );
 }
